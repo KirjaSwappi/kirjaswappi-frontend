89aaa8ac3f9ee62ca90c71fb1a073e8561cb36d0
rahat9016 <minhajurrohoman9016@gmail.com>
2025-08-04 00:40:43 +0600

    Message ui

--- src/pages/messages/Messages.tsx
+++ src/pages/messages/Messages.tsx
@@ -4,7 +4,7 @@ import ChatWindow from './components/ChatWindow';
 export default function Messages() {
   return (
     <div className="container flex gap-5 mt-6">
-      <aside className="w-[25%] bg-white">
+      <aside className="w-[25%] bg-white rounded-xl px-6 py-[30px]">
         <ChatList />
       </aside>
       <main className="w-[50%] bg-white rounded-xl">
