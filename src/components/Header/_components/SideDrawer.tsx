9277bbec24fcaf4363f38d86844fa3ba9cac3d7d
rahat9016 <minhajurrohoman9016@gmail.com>
2025-08-27 12:15:22 +0600

    Add: Filter Genre

--- src/components/Header/_components/SideDrawer.tsx
+++ src/components/Header/_components/SideDrawer.tsx
@@ -13,14 +13,14 @@ export default function SideDrawer({
         fixed inset-0 w-full h-screen bg-black bg-opacity-50
         transition-opacity duration-500 ease-in-out
         ${open ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}
-        z-50
+        z-[99999999]
       `}
     >
       <div
         className={`
           fixed top-0 ${left ? 'left-0' : 'right-0'} h-full bg-white px-4 py-8
           transition-transform duration-300 ease-in-out
-          ${open ? 'translate-x-0 w-[70%]' : left ? '-translate-x-full' : 'translate-x-full'}
+          ${open ? 'translate-x-0 w-[70%] lg:max-w-[280px]' : left ? '-translate-x-full' : 'translate-x-full'}
         `}
       >
         {children}
