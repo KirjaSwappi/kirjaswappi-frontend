fca1ef74d48e28a13e589ae7bfbbd081cf05522c
Minhazur Rahman <75428759+rahat9016@users.noreply.github.com>
2025-10-09 23:44:47 +0600

    Add Filter Books (#147)
    Co-authored-by: Minhazur Rahman <minhazurrahman@Minhazurs-MacBook-Air.local>
    Co-authored-by: rahat9016 <minhajurrohoman90116@gmail.com>

---
+++ src/components/Header/_components/BookFilter/FilterBySort.tsx
@@ -0,0 +1,57 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import { SortByEnum } from '../../../../utility/enum';
+import Button from '../../../shared/Button';
+import Line from '../../../shared/Line';
+
+const sortLabels: Record<SortByEnum, string> = {
+  [SortByEnum.title]: 'A-Z (Title)',
+  [SortByEnum.author]: 'Author (A-Z)',
+  //   [SortByEnum.language]: 'Language (A-Z)',
+  //   [SortByEnum.condition]: 'Condition (A-Z)',
+  [SortByEnum['genres.name']]: 'Genres (A-Z)',
+  [SortByEnum.bookUpdatedAt]: 'Newest First',
+};
+
+export default function FilterBySort() {
+  const { control } = useFormContext();
+
+  return (
+    <div>
+      <Controller
+        name="sortBy"
+        control={control}
+        render={({ field }) => (
+          <div className="py-2">
+            <div className="mt-2 space-y-0.5">
+              <span className="font-poppins font-normal text-sm px-4">Sort By</span>
+              <Line className="mt-6 mb-10" />
+              <div>
+                {Object.entries(sortLabels).map(([value, label]) => {
+                  const isChecked = field.value?.includes(value);
+                  return (
+                    <Button
+                      key={value}
+                      type="button"
+                      onClick={() => {
+                        if (isChecked) {
+                          field.onChange(field.value.filter((v: string) => v !== value));
+                        } else {
+                          field.onChange([...(field.value || []), value]);
+                        }
+                      }}
+                      className={`flex items-center justify-between gap-1 cursor-pointer w-full text-blackOlive font-poppins font-normal h-8 text-sm ${
+                        isChecked ? 'bg-primary text-white font-medium' : ''
+                      } px-4`}
+                    >
+                      <span className="pl-2">{label}</span>
+                    </Button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+}
