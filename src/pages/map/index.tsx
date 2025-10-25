0aad4a6a95ff3768bbfa12f5d176ceb889f9d739
rahat9016 <minhajurrohoman9016@gmail.com>
2025-10-25 18:10:16 +0600

    Filter added in the map

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -29,8 +29,8 @@ export const addLocationToBooks = (books: IBook[]): IBookWithLocation[] => {
 export default function Map() {
   useGeolocation();
   const navigate = useNavigate();
-  const { filter } = useAppSelector((state) => state.filter);
   const [booksWithLocation, setBooksWithLocation] = useState<IBookWithLocation[]>([]);
+  const { filter } = useAppSelector((state) => state.filter);
   const {
     userInformation: { id },
   } = useAppSelector((state) => state.auth);
