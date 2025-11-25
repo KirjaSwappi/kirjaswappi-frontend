93c81704286fe38b7dc2c84b53a2673bfee12c17
MoniruzzamanBillal <mdmoniruzzamanbillal@gmail.com>
2025-11-25 23:25:16 +0600

    feat:support-us page

---
+++ src/pages/support-us/components/SupportUsCard.tsx
@@ -0,0 +1,43 @@
+import { TSupportSection } from '..';
+import Button from '../../../components/shared/Button';
+
+type TPageProps = {
+  data: TSupportSection;
+};
+
+export default function SupportUsCard({ data }: TPageProps) {
+  console.log(data);
+
+  return (
+    <section className="w-full   ">
+      <div className="mx-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-center">
+          {/* Text Content */}
+          <div
+            className={`flex flex-col gap-6 ${data?.textOrder === 1 ? 'order-1  ' : 'order-2'} `}
+          >
+            <h1 className="text-2xl  font-bold "> {data?.title} </h1>
+            <p className="text-base text-blackOlive leading-relaxed">{data?.description}</p>
+            <div className=" mt-20 ">
+              <Button className="bg-primary hover:bg-blue-600 text-white px-6 py-2 rounded-md font-medium">
+                {data?.buttonText}
+              </Button>
+            </div>
+          </div>
+
+          {/* Image */}
+          <div className={`flex justify-center ${data?.textOrder === 1 ? 'order-2' : 'order-1'} `}>
+            <div className="relative w-full aspect-video rounded-lg overflow-hidden">
+              <img
+                src={data?.image}
+                // src="/public/images/supportUs/img1.jpg"
+                alt={data?.title}
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/support-us/index.tsx
+++ src/pages/support-us/index.tsx
@@ -1,12 +1,57 @@
+import SupportUsCard from './components/SupportUsCard';
+
+export type TSupportSection = {
+  id: number;
+  title: string;
+  description: string;
+  buttonText: string;
+  image: string;
+  textOrder: number;
+};
+
+export const supportSectionsData: TSupportSection[] = [
+  {
+    id: 1,
+    title: 'Donate',
+    description:
+      'Your financial contribution directly supports our programs and services, helping us reach more individuals and families in need. Every donation, no matter the size, makes a significant impact.',
+    buttonText: 'Make a Donation',
+    image: '/images/supportUs/img1.jpg',
+    textOrder: 1,
+  },
+  {
+    id: 2,
+    title: 'Collaboration',
+    description:
+      'Collaborate with us to amplify our impact and reach a wider audience. We welcome partnerships with businesses, organizations, and individuals who share our commitment to community support.',
+    buttonText: 'Move Forward',
+    image: '/images/supportUs/img2.jpg',
+    textOrder: 2,
+  },
+  {
+    id: 3,
+    title: 'Partner With Us',
+    description:
+      "Join us in making a difference by donating your time and skills. Volunteers play a vital role in supporting our programs and extending our reach in the community. Whether it's a few hours or regular commitment, your involvement matters.",
+    buttonText: 'Apply',
+    image: '/images/supportUs/img3.jpg',
+    textOrder: 1,
+  },
+];
+
 const SupportUs = () => {
   return (
-    <div>
-      <h1>support us</h1>
-      <h1>support us</h1>
-      <h1>support us</h1>
-      <h1>support us</h1>
-      <h1>support us</h1>
-      <h1>support us</h1>
+    <div className="font-poppins pb-24 lg:bg-white lg:container  mt-6  ">
+      <div className="pt-[56px] lg:max-w-[80%] lg:mx-auto lg:px-12  ">
+        {/*  */}
+        <div className=" flex flex-col gap-y-36 ">
+          {supportSectionsData?.map((item) => (
+            <SupportUsCard key={item?.id} data={item} />
+          ))}
+        </div>
+
+        {/*  */}
+      </div>
     </div>
   );
 };
