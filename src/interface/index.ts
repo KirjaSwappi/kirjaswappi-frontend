5842f813dedf3cb763501748363a87d02d7b6980
rahat9016 <minhajurrohoman9016@gmail.com>
2025-12-22 14:56:36 +0600

    Fix: type error in book filter

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -2,7 +2,7 @@ export interface IFilterData {
   genre: string[];
   condition: string[];
   language: string[];
-  sortBy: string[];
+  sortBy?: string[];
   search?: string;
   pageNumber?: number;
 }
